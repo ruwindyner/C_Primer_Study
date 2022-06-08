31983793b642c5e5f4fa9c450f01329afb865833
root <1738620999@qq.com>
2022-06-08 17:53:53 +0800

    又做了几道题

--- four_chapter/practice/02_printf_names.c
+++ four_chapter/practice/02_printf_names.c
@@ -5,16 +5,17 @@ int main(void)
 {
     char first_name[10];
     char last_name[10];
+    int len_first_name;
 
     printf("Pleast enter your first name\n");
     scanf("%s", first_name);
     printf("Pleast enter your last name\n");
     scanf("%s", last_name);
+    len_first_name = strlen(first_name);
     printf("\"%s,%s\"\n", first_name, last_name);
     printf("\"%-20s%s%s\"\n", "字段", first_name, last_name);
     printf("\"%s%s%20s\"\n", first_name, last_name,"字段");
-    printf("%*s%s\n", 3 * strlen(first_name) + strlen(last_name),first_name, last_name);
-
+    printf("%*s%s\n",len_first_name+3, first_name, last_name);
     return 0;
 
 }
